52510d947c59dfc669415cd9924e78ef9058ac0c
Victor <victor.fiamoncini@gmail.com>
2022-09-28 22:02:52 -0300

    refactor: add nullable currency values to convert-currency-page-controller and validate input values before convert

--- lib/presentation/controllers/convert_currency_page_controller.dart
+++ lib/presentation/controllers/convert_currency_page_controller.dart
@@ -3,39 +3,44 @@ import 'package:atv_flutter_03/application/entities/currency_entity.dart';
 import 'package:flutter/material.dart';
 
 class ConvertCurrencyPageController {
+  CurrencyEntity? fromCurrency;
+  CurrencyEntity? toCurrency;
+
   final TextEditingController fromTextEditingController;
   final TextEditingController toTextEditingController;
-
-  CurrencyEntity fromCurrency;
-  CurrencyEntity toCurrency;
-
   final CurrencyRepository currencyRepository;
 
   ConvertCurrencyPageController({
     required this.fromTextEditingController,
     required this.toTextEditingController,
-    required this.fromCurrency,
-    required this.toCurrency,
     required this.currencyRepository,
   });
 
+  void onFromCurrencyChange(CurrencyEntity? currency) {
+    fromCurrency = currency;
+  }
+
+  void onToCurrencyChange(CurrencyEntity? currency) {
+    toCurrency = currency;
+  }
+
   Future<void> convertCurrency() async {
-    try {
-      final inputValue = toTextEditingController.text.replaceAll(',', '.');
-      final parsedInputValue = double.parse(inputValue);
-
-      final convertionTypes = {
-        ConversionTypes.real: () => parsedInputValue * toCurrency.real,
-        ConversionTypes.dolar: () => parsedInputValue * toCurrency.dolar,
-        ConversionTypes.euro: () => parsedInputValue * toCurrency.euro,
-        ConversionTypes.bitcoin: () => parsedInputValue * toCurrency.bitcoin,
-      };
-
-      final convertedValue = convertionTypes[fromCurrency.name]!();
-
-      fromTextEditingController.text = convertedValue.toStringAsFixed(4);
-    } catch (e) {
-      throw 'Houve um erro ao converter, tente novamente';
+    if (fromCurrency is! CurrencyEntity || toCurrency is! CurrencyEntity) {
+      throw Exception('Houve um erro ao converter, tente novamente');
     }
+
+    final inputValue = toTextEditingController.text.replaceAll(',', '.');
+    final parsedInputValue = double.parse(inputValue);
+
+    final convertionTypes = {
+      ConversionTypes.real: () => parsedInputValue * toCurrency!.real,
+      ConversionTypes.dolar: () => parsedInputValue * toCurrency!.dolar,
+      ConversionTypes.euro: () => parsedInputValue * toCurrency!.euro,
+      ConversionTypes.bitcoin: () => parsedInputValue * toCurrency!.bitcoin,
+    };
+
+    final convertedValue = convertionTypes[fromCurrency?.name]!();
+
+    fromTextEditingController.text = convertedValue.toStringAsFixed(4);
   }
 }
